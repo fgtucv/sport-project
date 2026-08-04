8abcb58c299f1d0ab8770dd9f657cb258f906278
Oles Slava <oles.slava23@gmail.com>
2026-08-04 21:56:06 +0300

    Updates

--- src/components/Container/Container.jsx
+++
@@ -1,5 +0,0 @@
-export const Container = ({children}) => {
-    return <div style={{outline: "solid red", width: "1216px", margin: "0 auto"}} className="container">
-        {children}
-    </div>
-}
