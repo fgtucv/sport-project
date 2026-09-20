26c6a10c55f963c07faa597dd587951e2866d312
Oles Slava <oles.slava23@gmail.com>
2026-09-20 15:22:23 +0300

    some fixed HomePage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,4 +1,5 @@
-import { Component } from "react";
+import { useEffect, useState } from "react";
+import { useIsMobile } from "../../hooks/useIsMobile.js";
 import { Container } from "../../components/Container/Container.jsx";
 import { MainTitle } from "../../components/MainTitle/MainTitle.jsx";
 import { DateItem } from "./components/DateItem/DateItem.jsx";
@@ -8,54 +9,55 @@ import { GameItem } from "./components/GameItem/GameItem.jsx";
 import { GameMobileItem } from "./components/GameMobileItem/GameMobileItem.jsx";
 
 import style from "./HomePage.module.scss";
-
 import daysData from "../../data/days.json";
 
 const text = {
     title: "Доступні ігри",
     subtitle: "Знайдіть гру для себе щоб пограти",
-}
+};
 
-export class HomePage extends Component {
-    state = {
-        isMobile: window.matchMedia('(max-width: 1279px)'),
-        games: [],
-    }
+export const HomePage = () => {
+    const [games, setGames] = useState([]);
+    const isMobile = useIsMobile();
 
-    async componentDidMount() {
-        try {
-            const data = await fetch("https://6aa2acebccb3db9689a6e211.mockapi.io/game");
-            const gamesData = await data.json()
+    useEffect(() => {
+        const getGamesFromApi = async () => {
+            try {
+                const data = await fetch("https://6aa2acebccb3db9689a6e211.mockapi.io/game");
+                const gamesData = await data.json();
 
-            this.setState({
-                games: gamesData,
-            })
-        } catch (error) {
-            console.log(error);
-        }
-    }
+                setGames(gamesData);
+            } catch (error) {
+                console.log(error);
+            }
+        };
 
-    render() {
-        return (
-            <main className={style.main}>
-                <Container>
+        getGamesFromApi();
+    }, []);
 
-                    <MainTitle text={text} />
-                    <ul className={style.mainDates}>
-                        {daysData.map(day => <DateItem day={day} key={day.id} />)}
-                    </ul>
-                    <div className={style.mainFilter}>
-                        <Category />
-                        <Sorted />
-                    </div>
-                    <ul className={style.games}>
-                        {this.state.games.map((game) => (
-                            this.state.isMobile.matches ? <GameMobileItem key={game.id} obj={game} /> : <GameItem key={game.id} obj={game} />
-                        ))}
-                    </ul>
-
-                </Container>
-            </main>
-        )
-    }
+    return (
+        <main className={style.main}>
+            <Container>
+                <MainTitle text={text} />
+                <ul className={style.mainDates}>
+                    {daysData.map((day) => (
+                        <DateItem day={day} key={day.id} />
+                    ))}
+                </ul>
+                <div className={style.mainFilter}>
+                    <Category />
+                    <Sorted />
+                </div>
+                <ul className={style.games}>
+                    {games.map((game) =>
+                        isMobile ? (
+                            <GameMobileItem key={game.id} obj={game} />
+                        ) : (
+                            <GameItem key={game.id} obj={game} />
+                        )
+                    )}
+                </ul>
+            </Container>
+        </main>
+    );
 };
--- src/pages/HomePage/components/GameMobileItem/GameMobileItem.jsx
+++ src/pages/HomePage/components/GameMobileItem/GameMobileItem.jsx
@@ -1,46 +1,46 @@
 import { ComeToGameIcon, LocationIcon } from "../../../../components/Icons/Icons";
-
 import style from "./GameMobileItem.module.scss";
 
-const myLevel = 2.7;
+const myLevel = 2;
 
 export const GameMobileItem = ({ obj }) => {
-    const iSMyLevelUnavailable = myLevel <= obj.level && obj.levelRequirement === "strict" ? true : false;
+    const iSMyLevelUnavailable = myLevel <= obj.level && obj.isStrict ? true : false;
 
-    return (<li className={`${style.gameItem}  ${(iSMyLevelUnavailable || obj.isFull ? style.unavailable : style.available)}`}>
-        <div className={style.gameHeader}>
-            <div className={style.gameHeaderFlexGroup}>
-                <mark className={style.gameBadgeSport}>{obj.sportType === "PADEL" ? "Падель" : "Теніс"}</mark>
-                <mark className={style.gameBadgeLevel}>Level: {obj.level}</mark>
-                <span className={style.gamePriceValue}>{obj.price} €</span>
+    return (
+        <li className={`${style.gameItem}  ${(iSMyLevelUnavailable || obj.isFull ? style.unavailable : style.available)}`}>
+            <div className={style.gameHeader}>
+                <div className={style.gameHeaderFlexGroup}>
+                    <mark className={style.gameBadgeSport}>{obj.sportType}</mark>
+                    <mark className={style.gameBadgeLevel}>Level: {obj.level}</mark>
+                    <span className={style.gamePriceValue}>{obj.price / 4} €</span>
+                </div>
+                <time className={style.gameTime} dateTime={obj.time}>{obj.time} • {obj.durationMinutes} хв</time>
             </div>
-            <time className={style.gameTime} dateTime={obj.time}>{obj.time} • {obj.durationMinutes} хв</time>
-        </div>
 
-        <address className={style.gameLocation}>
-            <LocationIcon className={style.gameLocationIcon} />{obj.clubName}, {obj.courtNumber}
-        </address>
+            <address className={style.gameLocation}>
+                <LocationIcon className={style.gameLocationIcon} />{obj.clubName}, {obj.courtNumber}
+            </address>
 
-        <div className={style.gamePlayers}>
-            <figure className={style.gameAvatarsGroup}>
-                {obj.players.map((player) => (
-                    <img
-                        key={player.nickname}
-                        className={style.gameAvatarImg}
-                        src={player.avatarUrl}
-                        alt={`Гравець ${player.nickname}`}
-                    />
-                ))}
-                {obj.maxPlayers > obj.currentPlayers ? <button type="button" className={style.gameAddPlayerBtn} aria-label="Додати гравця"><ComeToGameIcon className={style.gameAddPlayerIcon} /></button> : ""}
-            </figure>
-            <span className={style.gamePlayersCount}>{obj.maxPlayers > obj.currentPlayers ? `${obj.currentPlayers} / ${obj.maxPlayers} гравців` : ""}</span>
-        </div>
+            <div className={style.gamePlayers}>
+                <figure className={style.gameAvatarsGroup}>
+                    {obj.players.map((player) => (
+                        <img
+                            key={player.nickname}
+                            className={style.gameAvatarImg}
+                            src={player.avatarUrl}
+                            alt={`Гравець ${player.nickname}`}
+                        />
+                    ))}
+                    {obj.maxPlayers > obj.currentPlayers ? <button type="button" className={style.gameAddPlayerBtn} aria-label="Додати гравця"><ComeToGameIcon className={style.gameAddPlayerIcon} /></button> : ""}
+                </figure>
+                <span className={style.gamePlayersCount}>{obj.maxPlayers > obj.currentPlayers ? `${obj.currentPlayers} / ${obj.maxPlayers} гравців` : ""}</span>
+            </div>
 
-        <div className={style.gameFooter}>
-            <span className={style.gameStrictLevel}>
-                {iSMyLevelUnavailable ? "У цієї гри строгий рівень" : obj.isFull ? "Забагато гравців" : ""}
-            </span>
-            <button type="button" className={style.gameJoinBtn}>{obj.isFull ? "Немає місць" : iSMyLevelUnavailable ? "Рівень занизький" : "Приєднатися"}</button>
-        </div>
-    </li>)
+            <div className={style.gameFooter}>
+                <span className={style.gameStrictLevel}>
+                    {iSMyLevelUnavailable ? "У цієї гри строгий рівень" : obj.isFull ? "Забагато гравців" : ""}
+                </span>
+                <button type="button" className={style.gameJoinBtn}>{obj.isFull ? "Немає місць" : iSMyLevelUnavailable ? "Рівень занизький" : "Приєднатися"}</button>
+            </div>
+        </li>)
 }
